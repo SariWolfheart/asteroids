4dce70f6f3995afab9a6f2b9e931035856f9f4ad
SariWolfheart <kaylanewcomb7@gmail.com>
2025-05-31 21:53:36 -0400

    added asteroid destruction and splitting

--- main.py
+++ main.py
@@ -35,6 +35,10 @@ def main():
             if asteroid.collision_check(player):
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.collision_check(shot):
+                    asteroid.split()
+                    shot.kill()
         for sprite in drawable:
             sprite.draw(screen)
         pygame.display.flip()
